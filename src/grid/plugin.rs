10fb45e94180168428d8c4413433d615b9eca54e
Andi Florea <alexflorea16000@gmail.com>
2023-01-29 17:11:03 +0200

    Cursor cell color

--- src/grid/plugin.rs
+++ src/grid/plugin.rs
@@ -1,5 +1,7 @@
 use bevy::{ecs::bundle, prelude::*};
 
+use crate::cursor::Cursor;
+
 #[derive(Bundle)]
 struct SquareBundle {
     #[bundle]
@@ -24,6 +26,7 @@ impl SquareBundle {
     }
 }
 
+#[derive(Component)]
 struct DebugGrid {
     pub position: Vec2,
     pub cell_size: f32,
@@ -60,6 +63,31 @@ impl DebugGrid {
             y as f32 * self.cell_size - self.cell_size / 2.,
         ) + self.cells_offset
     }
+
+    fn to_cell_coords(&self, pos: &Vec3) -> (usize, usize) {
+        let grid_width = self.cell_size * self.size_x as f32;
+        let grid_height = self.cell_size * self.size_y as f32;
+
+        let clamped_pos = Vec2::new(
+            pos.x.clamp(
+                self.position.x - grid_width / 2. - self.cell_size,
+                self.position.x + grid_width / 2. - self.cell_size,
+            ),
+            pos.y.clamp(
+                self.position.y - grid_height / 2. - self.cell_size,
+                self.position.y + grid_height / 2. - self.cell_size,
+            ),
+        );
+
+        let left_bottom =
+            self.to_screen_coords(0, 0) - Vec2::new(self.cell_size / 2., self.cell_size / 2.);
+        let current_offset = clamped_pos - left_bottom;
+
+        (
+            ((current_offset.x / self.cell_size as f32) as usize).clamp(0, self.size_x - 1),
+            ((current_offset.y / self.cell_size) as usize).clamp(0, self.size_y - 1),
+        )
+    }
 }
 
 fn spawn_grid(mut commands: Commands) {
@@ -78,6 +106,28 @@ fn spawn_grid(mut commands: Commands) {
             ));
         }
     }
+
+    commands.spawn((grid, Name::new("Grid")));
+}
+
+fn find_path(
+    cursor: Query<&Transform, With<Cursor>>,
+    grid: Query<&DebugGrid>,
+    mut nodes: Query<&mut DebugNode>,
+) {
+    for transform in cursor.iter() {
+        for grid in grid.iter() {
+            let cursor_coords = grid.to_cell_coords(&transform.translation);
+
+            for mut node in nodes.iter_mut() {
+                if node.x == cursor_coords.0 && node.y == cursor_coords.1 {
+                    node.color = Color::RED;
+                } else {
+                    node.color = Color::WHITE;
+                }
+            }
+        }
+    }
 }
 
 fn color_nodes(mut nodes: Query<(&mut Sprite, &DebugNode)>) {
@@ -90,6 +140,8 @@ pub struct GridPlugin;
 
 impl Plugin for GridPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_grid).add_system(color_nodes);
+        app.add_startup_system(spawn_grid)
+            .add_system(color_nodes)
+            .add_system(find_path);
     }
 }
