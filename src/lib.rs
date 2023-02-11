5394f40920bbbf455581bc774efaf731fd229fbf
Andi Florea <alexflorea16000@gmail.com>
2023-02-11 17:09:23 +0200

    Enemy setup cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -52,96 +52,70 @@ pub fn setup_entities(mut commands: Commands) {
     // ));
 }
 
-pub fn setup_enemy(mut commands: Commands, grid: Query<&DebugGrid>, mut debug_nodes: Query<&mut DebugNode>) {
-    // TODO: cleanup
+#[derive(Bundle)]
+struct EnemyBundle {
+    name: Name,
+    #[bundle]
+    sprite: SpriteBundle,
+    agent: GridAgent,
+}
+
+impl EnemyBundle {
+    fn new(pos: Vec3, path: Option<Vec<GridCoord>>) -> Self {
+        Self {
+            name: Name::new("Enemy"),
+            sprite: SpriteBundle {
+                sprite: Sprite {
+                    color: Color::RED,
+                    ..default()
+                },
+                transform: Transform::from_translation(pos).with_scale(Vec3::new(10., 10., 1.)),
+                ..default()
+            },
+            agent: GridAgent {
+                path,
+                speed: 50.,
+                error_margin: 0.5,
+            },
+        }
+    }
+}
+
+fn spawn_enemy(
+    commands: &mut Commands,
+    grid: &DebugGrid,
+    grid_pos: (usize, usize),
+    target: GridCoord,
+) {
+    let agent_pos = grid.to_screen_coords(grid_pos.0, grid_pos.1);
+    let agent_pos = Vec3::new(agent_pos.x, agent_pos.y, 10.);
+
+    let grid_pos = (grid_pos.0 as i32, grid_pos.1 as i32);
+    let path = grid.find_path(grid_pos, target);
+    commands.spawn(EnemyBundle::new(agent_pos, path));
+}
+
+pub fn setup_enemy(
+    mut commands: Commands,
+    grid: Query<&DebugGrid>,
+    mut debug_nodes: Query<&mut DebugNode>,
+) {
     let grid = grid.single();
     let target = (10, 3);
 
-    // set color of taret node
+    // set color of target node
     for mut node in debug_nodes.iter_mut() {
         if node.x == target.0 && node.y == target.1 {
             node.color = Color::GOLD;
         }
     }
-
     let target = (target.0 as i32, target.1 as i32);
+
     // spawn agents
-    let grid_pos = (0, 0);
-    let agent_pos = grid.to_screen_coords(grid_pos.0, grid_pos.1);
-    let agent_pos = Vec3::new(agent_pos.x, agent_pos.y, 10.);
-
-    let grid_pos = (grid_pos.0 as i32, grid_pos.1 as i32);
-    let path = grid.find_path(grid_pos, target);
-    commands.spawn((
-        Name::new("Enemy"),
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::RED,
-                ..default()
-            },
-            transform: Transform::from_translation(agent_pos).with_scale(Vec3::new(10., 10., 1.)),
-            ..default()
-        },
-        GridAgent { path, speed: 50., error_margin: 0.5 },
-    ));
-
-
-    let grid_pos = (0, 10);
-    let agent_pos = grid.to_screen_coords(grid_pos.0, grid_pos.1);
-    let agent_pos = Vec3::new(agent_pos.x, agent_pos.y, 10.);
-
-    let grid_pos = (grid_pos.0 as i32, grid_pos.1 as i32);
-    let path = grid.find_path(grid_pos, target);
-    commands.spawn((
-        Name::new("Enemy"),
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::RED,
-                ..default()
-            },
-            transform: Transform::from_translation(agent_pos).with_scale(Vec3::new(10., 10., 1.)),
-            ..default()
-        },
-        GridAgent { path, speed: 50., error_margin: 0.5 },
-    ));
-
-    let grid_pos = (15, 19);
-    let agent_pos = grid.to_screen_coords(grid_pos.0, grid_pos.1);
-    let agent_pos = Vec3::new(agent_pos.x, agent_pos.y, 10.);
-
-    let grid_pos = (grid_pos.0 as i32, grid_pos.1 as i32);
-    let path = grid.find_path(grid_pos, target);
-    commands.spawn((
-        Name::new("Enemy"),
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::RED,
-                ..default()
-            },
-            transform: Transform::from_translation(agent_pos).with_scale(Vec3::new(10., 10., 1.)),
-            ..default()
-        },
-        GridAgent { path, speed: 50., error_margin: 0.5 },
-    ));
-
-    let grid_pos = (19, 0);
-    let agent_pos = grid.to_screen_coords(grid_pos.0, grid_pos.1);
-    let agent_pos = Vec3::new(agent_pos.x, agent_pos.y, 10.);
-
-    let grid_pos = (grid_pos.0 as i32, grid_pos.1 as i32);
-    let path = grid.find_path(grid_pos, target);
-    commands.spawn((
-        Name::new("Enemy"),
-        SpriteBundle {
-            sprite: Sprite {
-                color: Color::RED,
-                ..default()
-            },
-            transform: Transform::from_translation(agent_pos).with_scale(Vec3::new(10., 10., 1.)),
-            ..default()
-        },
-        GridAgent { path, speed: 50., error_margin: 0.5 },
-    ));
+    spawn_enemy(&mut commands, &grid, (0, 0), target);
+    spawn_enemy(&mut commands, &grid, (0, 10), target);
+    spawn_enemy(&mut commands, &grid, (15, 19), target);
+    spawn_enemy(&mut commands, &grid, (19, 0), target);
 }
 
 // TODO: move this to the grid module
@@ -169,9 +143,12 @@ pub fn follow_path(
             if next_waypoint == None {
                 agent.path = None;
             } else if let Some(next_waypoint) = next_waypoint {
-                let target_pos = grid.to_screen_coords(next_waypoint.0 as usize, next_waypoint.1 as usize);
-                if transform.translation.x > target_pos.x - error_margin && transform.translation.x < target_pos.x + error_margin
-                    && transform.translation.y > target_pos.y - error_margin && transform.translation.y < target_pos.y + error_margin
+                let target_pos =
+                    grid.to_screen_coords(next_waypoint.0 as usize, next_waypoint.1 as usize);
+                if transform.translation.x > target_pos.x - error_margin
+                    && transform.translation.x < target_pos.x + error_margin
+                    && transform.translation.y > target_pos.y - error_margin
+                    && transform.translation.y < target_pos.y + error_margin
                 {
                     path.remove(0);
                     return;
